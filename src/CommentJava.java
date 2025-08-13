1faef56f6668d1a8ba6c793134e9d25c55089a50
leokim1178 <leo3179@naver.com>
2025-08-14 03:10:05 +0900

    java console 및 comment

---
+++ src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        // psvm으로 간단히 만들수 있음
+        System.out.println("hello java1"); // hello java1을 출력
+        // System.out.println("hello java2");
+
+        /* multi line comment
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+
+    }
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+
+    public static void main(String[] args) { // public static void가 시작점
+        // 들여쓰기는 4칸이 관례
+        System.out.println("hello java");
+        // 자바는 문자열을 사용할 때 쌍따옴표를 사용한다
+        // 세미콜론이 필수
+    }
+}
